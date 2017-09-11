869b6d65ba812e9b30fb2fd761ad3e09f06d1f76
Steve Witman <stevewitman@gmail.com>
2017-09-10 18:22:58 -0600

    Experiment with passing data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  private routeSub: any;
+
+  query: string;
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.routeSub = this.route.params.subscribe(params => {
+      console.log('Params;', params)
+      this.query = params['q']
+    })
+  }
+
+  ngOnDestroy() {
+    this.routeSub.unsubscribe()
+  }
 }
